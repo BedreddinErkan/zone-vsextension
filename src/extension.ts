66ccba967433b059615cf70d70e784841c827f44
Bedreddin ERKAN <bedreddinerkan@gmail.com>
2026-06-16 15:29:32 +0300

    fix(ext): add img-src data: to CSP so thumbnails render

--- src/extension.ts
+++ src/extension.ts
@@ -358,7 +358,7 @@ function getHtml(webview: vscode.Webview, extensionUri: vscode.Uri, nonce: strin
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
+  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
   <title>Zone</title>
   <style>
     :root {
